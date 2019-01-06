f7f24f39040ba157471d75c9b6884703585e476e
Tushar <tgirase@ptc.com>
2019-01-06 19:32:41 +0530

    Integrate log4j to application
    Added log4j and upated gitignore file

--- src/test/java/org/abcl/trainig/testng/AppTest.java
+++ src/test/java/org/abcl/trainig/testng/AppTest.java
@@ -1,7 +1,17 @@
 package org.abcl.trainig.testng;
 
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+
+    public static final Logger logger = Logger.getLogger(AppTest.class);
+
+    @Test
+    public void test() {
+        logger.info("Hello World !");
+    }
 }
